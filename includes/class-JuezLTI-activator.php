7ea495cf3b263c885c135b87e672c182272773f1
kok1974 <8818473@alu.murciaeduca.es>
2021-02-26 09:23:09 +0100

    inicio plugin de JuezLTI

---
+++ includes/class-JuezLTI-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    JuezLTI
+ * @subpackage JuezLTI/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    JuezLTI
+ * @subpackage JuezLTI/includes
+ * @author     Your Name <email@example.com>
+ */
+class JuezLTI_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
